e25ebfeb327875be3eae6596f3b54b2995d72a64
Pakorn <Pakorn.Thanaprachanon@outlook.com>
2022-05-05 22:13:30 +0700

    Fix fail import

--- scrape/app.py
+++ scrape/app.py
@@ -1,4 +1,5 @@
-import sched, time
+import sched
+import time
 from twint.TwintClass import TwintSearch
 from requests.ThaiRSCClass import ThaiRSCSearch
 from selenium.IticFoundationClass import IticFoundationSearch
@@ -6,17 +7,19 @@ from selenium.IticFoundationClass import IticFoundationSearch
 sched_object = sched.scheduler(time.time, time.sleep)
 
 API_ENDPOINT = "http://node-app:3000/api/v1/incidentsRaw/postIncident"
-username_target = ["js100radio", "Traffic_1197", "fm91trafficpro", "praramcommand"]
+username_target = ["js100radio", "Traffic_1197",
+                   "fm91trafficpro", "praramcommand"]
 search_string = ["รถชน", "ไฟไหม้", "อุบัติเหตุ", "ชนกัน",
                  "เพลิงไหม้", "ชนกับ", "ไฟไหม้", "เสียหลัก", "พุ่งชน"
-                ]
+                 ]
 
 username_target = ["js100radio"]
-search_string = ["อุบัติเหตุ",]
+search_string = ["อุบัติเหตุ", ]
 
 print("START AUTO Scrape")
 # scraping = TwintSearch(sched_object, 10, username_target, search_string)
-ThaiRSCSearch(sched_object, 3600, 0, 'http://www.thairsc.com/th/', 'BigAccidentAll.aspx?l=th')
+ThaiRSCSearch(sched_object, 3600, 0, 'http://www.thairsc.com/th/',
+              'BigAccidentAll.aspx?l=th')
 
 # IticFoundationSearch()
 
@@ -26,6 +29,3 @@ sched_object.run()
 # https://live.iticfoundation.org/# ❌
 # www.thairsc.com/th/ ✅
 # longdo api ❌
-
-
-
